658b12982720708425989741c4d149486450d7de
Sandra Vera <sandrav0609@gmail.com>
2019-09-29 11:45:34 -0400

    add tostring methods

---
+++ ProcessTable.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ProcessTable {
+    private HashMap<Integer, PCB> table; // <PID, PCB>
+
+    public synchronized PCB getProcess(int PID){ //No estoy segura si debe sere synchronized
+        return table.get(PID);
+    }
+
+    public synchronized void addProcess(int PID, PCB process){
+        table.put(PID, process);
+    }
+
+    public synchronized Boolean removeProcess(int PID){
+        PCB process = table.remove(PID);
+        if (process!=null) return true;
+        return false;
+    }
+
+    public void print(){
+        String s="";
+        table.values().forEach(pcb -> s+=pcb.toString()+"\n");
+        System.out.println(s);
+    }
+}
