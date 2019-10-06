f0872ec10d680f3bd4947e264793e39a44111f7d
Sandra Vera <sandrav0609@gmail.com>
2019-10-06 09:54:23 -0400

    agrega test rbtree monitor

--- Test.java
+++ Test.java
@@ -1,28 +1,47 @@
-
 import java.io.*;
 import java.util.*;
 
+// Clase de prueba para hilos
+class ThreadTest extends Thread{
+
+    RbTree tree;
+    int thread_no;
+
+    public ThreadTest(RbTree t, int number){
+        this.tree=t;
+        this.thread_no=number;
+        new Thread(this,"Thread "+number).start();
+    }
+
+    public void run(){
+        for (int i = 0; i < 5; i++) {
+            this.tree.insert(i);
+            System.out.println(thread_no+" inserta "+i);
+            try {
+                sleep((int)(Math.random() * 100));
+            } catch (InterruptedException e) { }
+        }
+        this.tree.print();
+        for (int i = 0; i < 5; i++) {
+            this.tree.delete(i);
+            System.out.println(thread_no+" elimina "+i);
+            try {
+                sleep((int)(Math.random() * 100));
+            } catch (InterruptedException e) { }
+        }
+        this.tree.print();
+    }
+
+}
 
 public class Test{
 
     public static void main(String [] args){
-
+        // Prueba RBTree
         RbTree t = new RbTree();
-        t.insert(10);
-        t.insert(9);
-        t.insert(8);
-        t.insert(7);
-        t.insert(6);
-        t.insert(5);
-        t.insert(4);
-        t.insert(3);
-        t.insert(2);
-        t.insert(1);
-        t.print();
-        t.delete(7);
-        t.delete(5);
-        t.delete(6);
-        t.print();
+        new ThreadTest(t, 0);
+        new ThreadTest(t, 1);
+        // Prueba process table
 
     }
 
