82e0189f62af81638c22c56474e18de3ff053d27
Sandra Vera <sandrav0609@gmail.com>
2019-10-08 20:18:54 -0400

    termina prioarray falta probarlo

--- Runqueue.java
+++ Runqueue.java
@@ -2,23 +2,46 @@ import java.util.*;
 
 public class PrioArray{
 
+    // Tabla 3-2 del libro
     int tasks_active; // Number of process descriptors linked into the lists
     BitSet bitmap; // Priority bitmap -> 1 if the priority list is not empty
-    ArrayList queue; // The 140 heads of the priority lists
+    ArrayList<LinkedList<ProcessDescriptor>> queue; // The 140 heads of the priority lists
 
     PrioArray(){
         this.bitmap = new BitSet(140);
-        this.queue = new ArrayList(140);
+        this.queue = new ArrayList<LinkedList<ProcessDescriptor>>(140);
+        this.tasks_active=0;
     }
 
-    // Faltan las funciones enqueue task y dequeue task de acuerdo a la prioridad
-    // essentially:
-    // list_add_tail(&p->run_list, &array->queue[p->prio]);
-    // __set_bit(p->prio, array->bitmap);
-    // array->nr_active++;
-    // p->array = array;
+    // Getter tasks_active
+    public int getTasksActive() {
+        return tasks_active;
+    }
+    
+    public void enqueue_task(ProcessDescriptor task){
+        int priority = task.getPrio();
+        // Check if reference is null to initialize list
+        LinkedList<ProcessDescriptor> list = this.queue.get(priority);
+        if (!list){
+            list = new LinkedList<ProcessDescriptor>();
+        }
+        list.add(task);
+        this.queue.get(priority) = list;
+        this.bitmap.set(priority);
+        tasks_active++;
+    }
 
-    // Faltan las funciones que modifiquen el bitmap
+    public void dequeue_task(ProcessDescriptor task){
+        int priority = task.getPrio();
+        // Check if task was last on list
+        LinkedList<ProcessDescriptor> list = this.queue.get(priority);
+        list.remove(task);
+        if (list.isEmpty()){
+            // Change bitmap
+            this.bitmap.set(priority, false);
+        }
+        tasks_active--;
+    }
 
 }
 
@@ -28,9 +51,16 @@ public class ProcessDescriptor{
     int state; // Estados posibles READY = 0, RUNNING = 1, BLOCKED = 2, TERMINATED = 3;
     int prio; // Priority -- TODO: definir si habra estatica y dinamica
     // LinkedList<> run_list; // Pointers to the next and prev elements in the runqueue list to which the process belongs
-    PrioArray prio_array; // PrioArray that includes the process
+    // PrioArray prio_array; // PrioArray that includes the process
     int time_slice; // Ticks left in the time quantum of the process
     int first_time_slice; // Flag, 1 if the process never exhausted its time quantum
+
+    ProcessDescriptor(int cpu, int prio){
+        this.cpu=cpu;
+        this.prio=prio;
+    }
+
+    // Faltan setters y getters
 
 }
 
