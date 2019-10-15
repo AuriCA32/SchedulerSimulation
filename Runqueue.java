0a083591abb4f0cc518c70097d4b86fad565876f
Sandra Vera <sandrav0609@gmail.com>
2019-10-15 13:45:19 -0400

    add prio_to_weight array for load calculations

--- Runqueue.java
+++ Runqueue.java
@@ -22,6 +22,17 @@ public class Runqueue{
     private final int TASK_SLEEP = 1; // Bloqueado
     private final int TASK_TERMINATED = 2;
 
+    private final int[] prio_to_weight = new int[]{
+        88761, 71755, 56483, 46273, 36291,
+        29154, 23254, 18705, 14949, 11916,
+        9548, 7620, 6100, 4904, 3906,
+        3121, 2501, 1991, 1586, 1277,
+        1024, 820, 655, 526, 423,
+        335, 272, 215, 172, 137,
+        110, 87, 70, 56, 45,
+        36, 29, 23, 18, 15
+        };
+
     Runqueue(int cpu){
         this.cpu_id = cpu;
         this.nr_running = 0;
