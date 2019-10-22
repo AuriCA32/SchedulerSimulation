fd09522e846d84b87ae040e6739906915314a9c0
Sandra Vera <sandrav0609@gmail.com>
2019-10-21 22:41:43 -0400

    important comments

--- CPU.java
+++ CPU.java
@@ -26,11 +26,14 @@ public class CPU{
         // rebalance_tick
     }
 
+    // insert process
+    // delete process after termination
+    // migrate process from runqueue migration list
     // try to wake up
     // recalc task prio
     // schedule
     void schedule(){
-
+        Task next = tree.
     }
     // load_balance, rebalance_tick, p.287
 
