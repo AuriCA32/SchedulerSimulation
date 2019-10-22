a65b721a17d848815df176589499185763d432bb
Sandra Vera <sandrav0609@gmail.com>
2019-10-21 20:50:43 -0400

    add clock

---
+++ Clock.java
@@ -0,0 +1,22 @@
+public class Clock implements Runnable{
+
+    int tick_duration; // in nanoseconds
+    int total_ticks;
+
+    public Clock(int tick) {
+        tick_duration = tick;
+        total_ticks = 0;
+    }
+
+    public void run() {
+        try {
+            sleep(tick_duration);
+            total_ticks++;
+        } catch (InterruptedException e) { }
+    }
+
+    public int checkTotalTicks(){
+        return total_ticks;
+    }
+
+}
